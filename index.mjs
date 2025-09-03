71dc852d78481a13c78b5137aad3f2d2be7159f6
João <joaociardullo@hotmail.com>
2025-09-03 18:19:20 -0300

    commit para git actions

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import { log } from './log.mjs';
 export const handler = async(event) => {
 
     log('Log de execução. event :' + JSON.stringify(event));
+    console.log('passei aqui')
 
     return {
         statusCode: 200
